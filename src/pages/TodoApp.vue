719d31a67e01b6b7e25578fb6dae16d885f35a77
ravi prajapati <prajapati.ravi2023@gmail.com>
2023-09-02 23:35:06 +0530

    update-task

--- src/pages/TodoApp.vue
+++ src/pages/TodoApp.vue
@@ -29,9 +29,19 @@
           class="bg-transparent text-white w-full p-2 border-b-2 border-white"
         />
         <i
+          class="fa-solid fa-pen mt-2 cursor-pointer"
+          @click="onUpdate"
+          v-if="isEditing"
+        >
+          <q-tooltip>Update Task</q-tooltip>
+        </i>
+        <i
           class="fa-solid fa-circle-plus text-xl mt-2 text-green-500 cursor-pointer"
           @click="newItem"
-        ></i>
+          v-if="!isEditing"
+        >
+          <q-tooltip>Add New Task</q-tooltip>
+        </i>
       </div>
       <ul class="task-list">
         <li
@@ -47,14 +57,19 @@
               :class="{ 'bg-green-500': task.checked }"
             />
             <span :class="{ 'line-through': task.checked }"
-              >{{ task.name }}
+              >{{ task.name }} {{ index }}
             </span>
           </label>
           <span class="ml-auto cursor-pointer">
+            <i class="fa-solid fa-eye mx-2" @click="editItem(task)">
+              <q-tooltip>Edit Task</q-tooltip>
+            </i>
             <i
               class="fa-solid fa-trash-can text-lg text-red-500"
               @click="delItem(index)"
-            ></i>
+            >
+              <q-tooltip>Delete Task</q-tooltip>
+            </i>
           </span>
         </li>
       </ul>
@@ -64,7 +79,9 @@
 <script setup>
 import { ref, computed } from "vue";
 const addnewtask = ref("");
-const id = ref(2);
+const id = ref(5);
+const editingTask = ref(null); // The task being edited
+const isEditing = ref(false);
 const tasks = ref([
   { name: "Start a new pen", id: 0, checked: false },
   { name: "Read a book", id: 1, checked: false },
@@ -73,26 +90,46 @@ const tasks = ref([
 
 const newItem = () => {
   if (addnewtask.value) {
-    tasks.value.push({ name: addnewtask.value, id: id.value > id.value + 1 });
+    tasks.value.push({ name: addnewtask.value, id: id.value });
+    id.value++; // Increment id for the next task
   }
   addnewtask.value = "";
 };
+
+const editItem = (item) => {
+  addnewtask.value = item.name;
+  id.value = item.id;
+  isEditing.value = true;
+  editingTask.value = item;
+};
+
+const onUpdate = () => {
+  if (isEditing.value && addnewtask.value) {
+    // Find the task by its ID and update it
+    const updatedTaskIndex = tasks.value.findIndex(
+      (task) => task.id === editingTask.value.id
+    );
+    if (updatedTaskIndex !== -1) {
+      tasks.value[updatedTaskIndex].name = addnewtask.value;
+    }
+    isEditing.value = false; // Turn off editing mode
+    addnewtask.value = ""; // Clear the input field
+  }
+};
+
 const delItem = (index) => {
   tasks.value.splice(index, 1);
 };
+
 const totalTask = computed(() => {
-  const totalTaskcount = tasks.value.length;
-  return totalTaskcount;
+  return tasks.value.length;
 });
+
 const completedTask = computed(() => {
-  const totalcompletedTask = tasks.value.filter(
-    (x) => x.checked === true
-  ).length;
-  return totalcompletedTask;
+  return tasks.value.filter((x) => x.checked === true).length;
 });
+
 const uncompletedTask = computed(() => {
-  const totalUncompletedTask =
-    parseFloat(totalTask.value) - parseFloat(completedTask.value);
-  return totalUncompletedTask;
+  return totalTask.value - completedTask.value;
 });
 </script>
